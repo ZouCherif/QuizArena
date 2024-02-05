eb772fd3f9c24b64d03ff23db4fb3f9ad45cdbe2
Zouaoui Mohamed Cherif <cheriffzouaoui@gmail.com>
2024-02-05 16:44:41 +0100

    working on a resonsive navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -11,41 +11,48 @@ function NavBar() {
   const [showRegister, setShowRegister] = useState(false);
 
   return (
-    <div className="bg-[#1A1A2F] px-2 py-3">
+    <div className="bg-[#1A1A2F] lg:px-2 lg:py-3 py-2">
       <div
-        className={`flex max-w-[1500px] m-auto px-8 items-center justify-between ${
+        className={`flex max-w-[1500px] m-auto lg:px-8 px-2 items-center justify-between ${
           showRegister || ShowSignIn ? "select-none" : ""
         }`}
       >
         <div className="flex items-center">
-          <div className="text-xl font-semibold mr-8 select-none">
-            <span className="text-2xl text-[#6541F5] font-bold">Q</span>uiz
-            <span className="text-2xl font-bold text-orange-500">A</span>
+          <div className="lg:text-xl text-lg font-semibold lg:mr-8 md:mr-4 mr-2 select-none">
+            <span className="lg:text-2xl text-xl text-[#6541F5] font-bold">
+              Q
+            </span>
+            uiz
+            <span className="lg:text-2xl text-xl font-bold text-orange-500">
+              A
+            </span>
             rena
           </div>
-          <div className="flex flex-initial w-96 rounded-full items-center bg-[#DADADA] py-1 px-4 mr-4">
+          <div className="flex flex-initial lg:w-96 w-60 rounded-full items-center bg-[#DADADA] py-1 md:px-4 px-2 lg:mr-4 mr-2">
             <input
               type="text"
-              className="bg-transparent outline-none mr-2 flex-1 text-black"
+              className="bg-transparent outline-none md:mr-2 flex-1 text-black lg:text-base md:text-sm text-xs"
               placeholder="Trouver un quiz"
             />
-            <FaSearch size={20} className="*:text-gray-400" />
+            <FaSearch size={16} className="*:text-gray-400" />
           </div>
-          <button className="sm:flex items-center bg-orange-600 hover:bg-gray-700 duration-500 py-1 pr-4 pl-2 rounded-lg hidden   ">
-            <IoMdAdd className="mr-2" size={20} />
-            Creer un quiz
-          </button>
+          {user && (
+            <button className="sm:flex items-center bg-orange-600 hover:bg-gray-700 duration-500 py-1 md:pr-4 md:pl-2 pl-1 pr-2 rounded-lg hidden whitespace-nowrap lg:text-base md:text-sm text-xs">
+              <IoMdAdd className="md:mr-2" size={15} />
+              Creer un quiz
+            </button>
+          )}
         </div>
         {!user ? (
-          <div className="flex">
+          <div className="sm:flex hidden whitespace-nowrap">
             <button
-              className="rounded-lg bg-gray-700 mr-4 px-4 py-1 hover:bg-gray-500 duration-300 sm:block hidden"
+              className="rounded-lg bg-gray-700 md:mr-4 mr-2 md:px-4 px-2 py-1 hover:bg-gray-500 duration-300 lg:text-base md:text-sm text-xs"
               onClick={() => setShowRegister(true)}
             >
               S'inscrire
             </button>
             <button
-              className="rounded-lg bg-[#6541F5] px-4 py-1 hover:bg-[#886df3] duration-300 sm:block hidden"
+              className="rounded-lg bg-[#6541F5] md:px-4 px-2 py-1 hover:bg-[#886df3] duration-300 lg:text-base md:text-sm text-xs"
               onClick={() => setShowSignIn(true)}
             >
               Se connecter
