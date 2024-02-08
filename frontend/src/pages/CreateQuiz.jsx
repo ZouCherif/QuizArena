8c74dcdbc27e07bbd8d1756fe63eb1648bd0d89d
Zouaoui Mohamed Cherif <cheriffzouaoui@gmail.com>
2024-02-08 15:51:45 +0100

    createQuiz page 80%

--- frontend/src/pages/CreateQuiz.jsx
+++ frontend/src/pages/CreateQuiz.jsx
@@ -1,7 +1,42 @@
+import { useState } from "react";
 function CreateQuiz() {
+  const [isChecked, setIsChecked] = useState(true);
+  const [categories, setCategories] = useState([]);
+  const cat = [
+    "Mathematiques",
+    "Histoire geo",
+    "Langues",
+    "Culture generale",
+    "Science",
+    "Sport",
+    "Art",
+    "Musique",
+    "TV & Films",
+  ];
+
+  const handleRadio = (e) => {
+    if (e.target.id === "auto") {
+      setIsChecked(true);
+    } else {
+      setIsChecked(false);
+    }
+  };
+
+  const handleCategories = (e) => {
+    const categoryName = e.target.getAttribute("data-name");
+    console.log(categoryName);
+    if (!categories.includes(categoryName)) {
+      setCategories([...categories, categoryName]);
+    } else {
+      const updatedCategories = categories.filter(
+        (category) => category !== categoryName
+      );
+      setCategories(updatedCategories);
+    }
+  };
   return (
-    <div className="bg-[#1A1A2F] fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-lg z-20 sm:w-[600px] w-screen sm:h-5/6 h-screen overflow-auto px-10 py-4">
-      <div className="flex justify-between items-center mb-8">
+    <div className="bg-[#1A1A2F] fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-lg z-20 sm:w-[700px] w-screen sm:h-5/6 h-screen overflow-auto px-10 py-4">
+      <div className="flex justify-between items-center mb-4">
         <h1 className="text-4xl mt-2 font-semibold">Creer un quiz</h1>
         <select
           defaultValue={"publique"}
@@ -15,7 +50,7 @@ function CreateQuiz() {
           </option>
         </select>
       </div>
-      <div className="w-2/3 mx-auto">
+      <div className="w-4/5 mx-auto">
         <label htmlFor="name">nom du quiz:</label>
         <input
           type="text"
@@ -25,11 +60,12 @@ function CreateQuiz() {
           placeholder="Le fameux quiz"
           required
         />
-        <div className="flex">
-          <div>
+        <div className="flex mb-2">
+          <div className="flex-1">
             <label htmlFor="nbP" className="text-sm">
               nombre de joueur max:
             </label>
+            <br />
             <input
               type="number"
               id="nbP"
@@ -39,10 +75,11 @@ function CreateQuiz() {
               required
             />
           </div>
-          <div>
+          <div className="flex-1">
             <label htmlFor="nbQ" className="text-sm">
               nombre de question:
             </label>
+            <br />
             <input
               type="number"
               id="nbQ"
@@ -50,8 +87,78 @@ function CreateQuiz() {
               className="w-[100px] p-3 rounded bg-[#D9D9D9] opacity-50 text-black mb-1"
               placeholder="15"
               required
+              disabled={!isChecked}
             />
           </div>
+        </div>
+        <div className="flex mb-2">
+          <div className="flex-1">
+            <input
+              id="auto"
+              name="questions"
+              type="radio"
+              className="align-middle mr-1"
+              defaultChecked={true}
+              onChange={handleRadio}
+            />
+            <label htmlFor="auto" className="text-sm mr-2 cursor-pointer">
+              generer mes questions
+            </label>
+          </div>
+          <div className="flex-1">
+            <input
+              id="manual"
+              name="questions"
+              type="radio"
+              className="align-middle mr-1"
+              onChange={handleRadio}
+            />
+            <label htmlFor="manual" className="text-sm cursor-pointer">
+              je cree mes questions
+            </label>
+          </div>
+        </div>
+
+        <div
+          className={`duration-200 ${
+            isChecked ? "" : "opacity-10 select-none pointer-events-none"
+          }`}
+        >
+          <p className="text-sm mb-1">
+            selectionnez le niveau de difficulter du quiz:
+          </p>
+          <select className="bg-[#1A1A2F] border rounded-lg p-2 mb-1">
+            <option value="facile" className="text-sm">
+              Facile
+            </option>
+            <option value="moyen" className="text-sm">
+              Moyen
+            </option>
+            <option value="difficile" className="text-sm">
+              Difficile
+            </option>
+          </select>
+          <p className="text-sm mb-1">
+            Sélectionnez les catégories souhaitées:
+          </p>
+          <ul className="flex flex-wrap text-sm whitespace-nowrap">
+            {cat.map((category) => {
+              return (
+                <li
+                  className={`mr-2 border rounded-full py-1 px-2 mb-1 cursor-pointer duration-300 ${
+                    categories.includes(category)
+                      ? "bg-[#6541F5] hover:bg-[#492bc3]"
+                      : "hover:bg-gray-500"
+                  }`}
+                  data-name={category}
+                  onClick={handleCategories}
+                  key={category}
+                >
+                  {category}
+                </li>
+              );
+            })}
+          </ul>
         </div>
       </div>
       <button
